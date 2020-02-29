62baae7dd7cc6696d482a5fada87e5fb95e39bca
alper <alper.usss@gmail.com>
2020-02-29 15:57:42 +0300

    ok

---
+++ ECommerce/ECommerce/ECommerce.Data/Migrations/20200229125507_Initial5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.Data.Migrations
+{
+    public partial class Initial5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 2, 29, 12, 55, 7, 773, DateTimeKind.Utc));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 2, 23, 14, 15, 24, 106, DateTimeKind.Utc));
+        }
+    }
+}
--- ECommerce/ECommerce/ECommerce.Web/Controllers/UserController.cs
+++ ECommerce/ECommerce/ECommerce.Web/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Data.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Web.Controllers
@@ -26,12 +27,20 @@ namespace ECommerce.Web.Controllers
             }
 
             var user=_unitOfWork.UserRepository.GetByEmailAndPassword(user_LoginAction_Request.Email, user_LoginAction_Request.Password);
-            //TODO:buraya gelecez.
+            
+
             if (user==null)
             {
                 return Unauthorized();
             }
+
+            else
+            {
+                HttpContext.Session.SetInt32("UserId",user.Id);
+            }
+
             return new JsonResult(user);
+
         }
     }
 }
